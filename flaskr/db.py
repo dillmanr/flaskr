4e4507257aaa5ebff9749c4b18aea328e5a1a06a
Ryan Dillman <33791780+dillmanr@users.noreply.github.com>
2020-06-04 22:28:38 -0400

    initialized database

--- flaskr/db.py
+++ flaskr/db.py
@@ -25,7 +25,7 @@ def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read.decode('utf8'))
+        db.executescript(f.read().decode('utf8'))
 
 @click.command('init-db')
 @with_appcontext
@@ -33,4 +33,8 @@ def init_db_command():
     """Clear existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
